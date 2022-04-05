59c2d76fadfc5e638468dbab423be2fd038b9906
Charles Lowell <cowboyd@frontside.com>
2022-04-05 14:57:24 +0300

    Get trivial tests working, apply graphql modules

--- plugins/graphql/src/app/index.ts
+++ plugins/graphql/src/app/index.ts
@@ -1,7 +1,10 @@
 import type { Catalog } from './catalog';
-import { GetEnvelopedFn, useSchema, envelop, useExtendContext } from '@envelop/core';
+import { GetEnvelopedFn, envelop, useExtendContext } from '@envelop/core';
+import { useGraphQLModules } from '@envelop/graphql-modules';
 import { Application, createApplication } from 'graphql-modules';
 import { ResolverContext } from './resolver-context';
+import { Relay } from './modules/relay';
+import { Entities } from './modules/entities';
 
 export interface App {
   (): ReturnType<GetEnvelopedFn<ResolverContext>>;
@@ -11,12 +14,11 @@ export const schema = create().schema;
 
 export function createApp(catalog: Catalog): App {
   const application = create();
-  const schema = application.createSchemaForApollo();
 
   const run = envelop({
     plugins: [
       useExtendContext(() => ({ catalog })),
-      useSchema(schema),
+      useGraphQLModules(application),
     ],
   });
 
@@ -25,6 +27,6 @@ export function createApp(catalog: Catalog): App {
 
 function create(): Application {
   return createApplication({
-    modules: [],
+    modules: [Relay, Entities],
   });
 }
