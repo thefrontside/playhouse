9ba798b48adaa71f9f8f2973128c664be033c844
wKich <w@kich.dev>
2022-04-13 10:29:26 +0500

    Add ield and hasOne directives

--- plugins/graphql/src/app/modules/component.ts
+++ plugins/graphql/src/app/modules/component.ts
@@ -1,14 +1,7 @@
+import { ComponentEntity, Entity } from '@backstage/catalog-model';
 import { createModule, gql } from 'graphql-modules';
-import {
-  id,
-  name,
-  namespace,
-  title,
-  description,
-  tags,
-  links,
-} from '../resolvers';
-import { entityToNode } from '../mappers'
+import { pascalCase } from 'pascal-case'
+import { resolverProvider } from '../resolver';
 
 export const Component = createModule({
   id: `component`,
@@ -19,36 +12,96 @@ export const Component = createModule({
       DEPRECATED
     }
 
-    type Component implements Node & Entity {
+    interface Component {
+      lifecycle: Lifecycle! @field(at: "spec.lifecycle")
+      owner: Owner! @hasOne(type: "ownedBy")
+      subcomponentOf: Component @hasOne(type: "partOf")
+      components: [Component] # Use ComponentConnection
+      # providesApis: [API] #@field(at: "spec.providesApis")
+      # consumesApis: [API] #@field(at: "spec.consumesApis")
+      dependencies: [Resource] #@field(at: "spec.dependsOn")
+      system: System #@field(at: "spec.system")
+    }
+
+    type Website implements Node & Entity & Component {
       id: ID!
+
       name: String!
       namespace: String
       title: String
       description: String
-      tags: [String]
-      links: [EntityLink]
+      tags: [String!]
+      links: [EntityLink!]
 
-      type: String! #@field(at: "spec.type")
-      lifecycle: Lifecycle! #@field(at: "spec.lifecycle")
-      owner: Owner! #@field(at: "spec.owner")
-      # subcomponentOf: Component #@field(at: "spec.subcomponentOf")
-      # components: [Component] # Use ComponentConnection
-      # providesApis: [API] #@field(at: "spec.providesApis")
-      # consumesApis: [API] #@field(at: "spec.consumesApis")
-      # dependencies: [Resource] #@field(at: "spec.dependsOn")
-      # system: System #@field(at: "spec.system")
+      lifecycle: Lifecycle!
+      owner: Owner!
+      subcomponentOf: Component
+      components: [Component]
+      # providesApis: [API]
+      # consumesApis: [API]
+      dependencies: [Resource]
+      system: System
     }
+
+    type Service implements Node & Entity & Component {
+      id: ID!
+
+      name: String!
+      namespace: String
+      title: String
+      description: String
+      tags: [String!]
+      links: [EntityLink!]
+
+      lifecycle: Lifecycle!
+      owner: Owner!
+      subcomponentOf: Component
+      components: [Component]
+      # providesApis: [API]
+      # consumesApis: [API]
+      dependencies: [Resource]
+      system: System
+    }
+
+    type Library implements Node & Entity & Component {
+      id: ID!
+
+      name: String!
+      namespace: String
+      title: String
+      description: String
+      tags: [String!]
+      links: [EntityLink!]
+
+      lifecycle: Lifecycle!
+      owner: Owner!
+      subcomponentOf: Component
+      components: [Component]
+      # providesApis: [API]
+      # consumesApis: [API]
+      dependencies: [Resource]
+      system: System
+    }
+
+    # service, library
   `,
-  // providers: [entityToNode()]
-  // resolvers: {
-  //   Component: {
-  //     id,
-  //     name,
-  //     namespace,
-  //     title,
-  //     description,
-  //     tags,
-  //     links,
-  //   },
-  // },
+  resolvers: {
+    Lifecycle: {
+      EXPERIMENTAL: 'experimental',
+      PRODUCTION: 'production',
+      DEPRECATED: 'deprecated',
+    },
+    // Component: {
+    //   __resolveType: async ({ id }: { id: string }, { loader }: ResolverContext): Promise<string | null> => {
+    //     const entity = await loader.load(id);
+    //     return (entity ? entity.__typeName : 'Unknown') ?? null;
+    //   }
+    // }
+  },
+  providers: [
+    resolverProvider({
+      accept: (entity: Entity): entity is ComponentEntity => entity.kind === 'Component',
+      resolve: entity => entity ? ({ __typeName: pascalCase(entity.spec.type), ...entity }) : null,
+    }),
+  ],
 });
--- plugins/graphql/src/app/modules/user.ts
+++ plugins/graphql/src/app/modules/user.ts
@@ -1,14 +1,29 @@
+import { Entity, GroupEntity, UserEntity } from '@backstage/catalog-model';
 import { createModule, gql } from 'graphql-modules';
+import { pascalCase } from 'pascal-case'
+import { resolverProvider } from '../resolver';
+import { ResolverContext } from '../resolver-context';
 
 export const User = createModule({
   id: `user`,
   typeDefs: gql`
-    union Owner = User | Group
+    union Owner = User | Team | SubDepartment | Department | Organization
     # union Ownable = System | Resource | Component | API | Domain | Template
-    union Ownable = System | Resource | Component | Domain
+    union Ownable = System | Resource | Website | Service | Library | Domain
 
-    type Group implements Node & Entity {
+    interface Group {
+      children: [Group]! #@field(at: "spec.children")
+      displayName: String @field(at: "spec.profile.displayName")
+      email: String @field(at: "spec.profile.email")
+      picture: String @field(at: "spec.profile.picture")
+      parent: Group @hasOne(type: "childOf")
+      members: [User] #@field(at: "spec.members")
+      owns: [Ownable]
+    }
+
+    type Team implements Node & Entity & Group {
       id: ID!
+
       name: String!
       namespace: String
       title: String
@@ -16,13 +31,69 @@ export const User = createModule({
       tags: [String]
       links: [EntityLink]
 
-      type: String! #@field(at: "spec.type")
-      children: [Group]! #@field(at: "spec.children")
-      displayName: String #@field(at: "spec.profile.displayName")
-      email: String #@field(at: "spec.profile.email")
-      picture: String #@field(at: "spec.profile.picture")
-      parent: Group #@field(at: "spec.parent")
-      members: [User] #@field(at: "spec.members")
+      children: [Group]!
+      displayName: String
+      email: String
+      picture: String
+      parent: SubDepartment
+      members: [User]
+      owns: [Ownable]
+    }
+
+    type SubDepartment implements Node & Entity & Group {
+      id: ID!
+
+      name: String!
+      namespace: String
+      title: String
+      description: String
+      tags: [String]
+      links: [EntityLink]
+
+      children: [Group]!
+      displayName: String
+      email: String
+      picture: String
+      parent: Department
+      members: [User]
+      owns: [Ownable]
+    }
+
+    type Department implements Node & Entity & Group {
+      id: ID!
+
+      name: String!
+      namespace: String
+      title: String
+      description: String
+      tags: [String]
+      links: [EntityLink]
+
+      children: [Group]!
+      displayName: String
+      email: String
+      picture: String
+      parent: Organization
+      members: [User]
+      owns: [Ownable]
+    }
+
+    type Organization implements Node & Entity & Group {
+      id: ID!
+
+      name: String!
+      namespace: String
+      title: String
+      description: String
+      tags: [String]
+      links: [EntityLink]
+
+      children: [Group]!
+      displayName: String
+      email: String
+      picture: String
+      parent: Group
+      members: [User]
       owns: [Ownable]
     }
 
@@ -36,14 +107,29 @@ export const User = createModule({
       links: [EntityLink]
 
       memberOf: [Group]! #@field(at: "spec.memberOf")
-      displayName: String #@field(at: "spec.profile.displayName")
-      email: String #@field(at: "spec.profile.email")
-      picture: String #@field(at: "spec.profile.picture")
+      displayName: String @field(at: "spec.profile.displayName")
+      email: String @field(at: "spec.profile.email")
+      picture: String @field(at: "spec.profile.picture")
       owns: [Ownable]
     }
-
-    extend type Query {
-      owner(kind: String!, name: String!, namespace: String): Owner
-    }
   `,
+  resolvers: {
+    Owner: {
+      __resolveType: async ({ id }: { id: string }, { loader }: ResolverContext): Promise<string | null> => {
+        const entity = await loader.load(id);
+        return (entity ? entity.__typeName : 'Unknown') ?? null;
+      }
+    }
+    // TODO Ownable
+  },
+  providers: [
+    resolverProvider({
+      accept: (entity: Entity): entity is GroupEntity => entity.kind === 'Group',
+      resolve: entity => entity ? ({ __typeName: pascalCase(entity.spec.type), ...entity }) : null,
+    }),
+    resolverProvider({
+      accept: (entity: Entity): entity is UserEntity => entity.kind === 'User',
+      resolve: entity => entity ? ({ __typeName: 'User', ...entity }) : null,
+    }),
+  ],
 });
