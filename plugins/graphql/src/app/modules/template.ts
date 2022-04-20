60092314295337dc11380192cbfc1fbda1d892a1
wKich <w@kich.dev>
2022-04-20 19:05:50 +0500

    Add graphql code generation

--- plugins/graphql/src/app/modules/template.ts
+++ plugins/graphql/src/app/modules/template.ts
@@ -1,79 +1,14 @@
+import { resolvePackagePath } from "@backstage/backend-common";
 import { Entity, TemplateEntityV1beta2 } from "@backstage/catalog-model";
-import { createModule, gql } from "graphql-modules";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { createModule } from "graphql-modules";
 import GraphQLJSON, { GraphQLJSONObject } from 'graphql-type-json';
 import { pascalCase } from 'pascal-case'
 import { resolverProvider } from "../resolver";
 
 export const Template = createModule({
   id: 'Template',
-  typeDefs: gql`
-    scalar JSON
-    scalar JSONObject
-    union Ownable = Website | Documentation | Service
-
-    type Step {
-      id: String
-      name: String
-      action: String!
-      input: JSONObject
-      if: JSON
-    }
-
-    interface Template {
-      parameters: JSONObject @field(at: "spec.parameters ")
-      steps: [Step]! @field(at: "spec.steps")
-      output: JSONObject @field(at: "spec.output")
-      owner: Owner @relation(type: "ownedBy")
-    }
-
-    type Website implements Node & Entity & Template {
-      id: ID!
-
-      name: String!
-      namespace: String
-      title: String
-      description: String
-      tags: [String!]
-      links: [EntityLink!]
-
-      parameters: JSONObject
-      steps: [Step]!
-      output: JSONObject
-      owner: Owner
-    }
-
-    type Documentation implements Node & Entity & Template {
-      id: ID!
-
-      name: String!
-      namespace: String
-      title: String
-      description: String
-      tags: [String!]
-      links: [EntityLink!]
-
-      parameters: JSONObject
-      steps: [Step]!
-      output: JSONObject
-      owner: Owner
-    }
-
-    type Service implements Node & Entity & Template {
-      id: ID!
-
-      name: String!
-      namespace: String
-      title: String
-      description: String
-      tags: [String!]
-      links: [EntityLink!]
-
-      parameters: JSONObject
-      steps: [Step]!
-      output: JSONObject
-      owner: Owner
-    }
-  `,
+  typeDefs: loadFilesSync(resolvePackagePath('@internal/plugin-graphql', 'typedefs/template.graphql')),
   resolvers: {
     JSON: GraphQLJSON,
     JSONObject: GraphQLJSONObject,
