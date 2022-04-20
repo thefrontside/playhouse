af72043cbb6601dcd94aee5d7c834432aaf72ad1
wKich <w@kich.dev>
2022-04-20 19:38:08 +0500

    Remove resolve provider

--- plugins/graphql/src/app/modules/system.ts
+++ plugins/graphql/src/app/modules/system.ts
@@ -1,16 +1,8 @@
 import { resolvePackagePath } from '@backstage/backend-common';
-import { Entity, SystemEntity } from '@backstage/catalog-model';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { createModule } from 'graphql-modules';
-import { resolverProvider } from '../resolver';
 
 export const System = createModule({
   id: `system`,
   typeDefs: loadFilesSync(resolvePackagePath('@internal/plugin-graphql', 'typedefs/system.graphql')),
-  providers: [
-    resolverProvider({
-      accept: (entity: Entity): entity is SystemEntity => entity.kind === 'System',
-      resolve: entity => entity ? ({ __typeName: 'System', ...entity }) : null,
-    }),
-  ],
 });
