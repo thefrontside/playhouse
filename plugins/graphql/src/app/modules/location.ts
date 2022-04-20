b83b41aa22fc1bb84c2828cd774512e7283c5fe1
wKich <w@kich.dev>
2022-04-20 19:05:50 +0500

    Add graphql code generation

--- plugins/graphql/src/app/modules/location.ts
+++ plugins/graphql/src/app/modules/location.ts
@@ -1,25 +1,12 @@
+import { resolvePackagePath } from "@backstage/backend-common";
 import { Entity, LocationEntity } from "@backstage/catalog-model";
-import { createModule, gql } from "graphql-modules";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { createModule } from "graphql-modules";
 import { resolverProvider } from "../resolver";
 
 export const Location = createModule({
   id: 'location',
-  typeDefs: gql`
-    type Location implements Node & Entity {
-      id: ID!
-
-      name: String!
-      namespace: String
-      title: String
-      description: String
-      tags: [String!]
-      links: [EntityLink!]
-
-      type: String @field(at: "spec.type")
-      target: String @field(at: "spec.target")
-      targets: [String] @field(at: "spec.targets")
-    }
-  `,
+  typeDefs: loadFilesSync(resolvePackagePath('@internal/plugin-graphql', 'typedefs/location.graphql')),
   providers: [
     resolverProvider({
       accept: (entity: Entity): entity is LocationEntity => entity.kind === 'Location',
