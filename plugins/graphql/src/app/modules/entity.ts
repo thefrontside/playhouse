88402965171d8cf05d0055fbd54c961dc00fbd76
wKich <w@kich.dev>
2022-04-20 19:05:50 +0500

    Add graphql code generation

--- plugins/graphql/src/app/modules/entity.ts
+++ plugins/graphql/src/app/modules/entity.ts
@@ -1,28 +1,11 @@
-import { createModule, gql } from 'graphql-modules';
+import { resolvePackagePath } from '@backstage/backend-common';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { createModule } from 'graphql-modules';
 import { encodeId } from '../loaders';
 
 export const Entity = createModule({
   id: 'entity',
-  typeDefs: gql`
-    interface Entity {
-      name: String! @field(at: "metadata.name")
-      namespace: String @field(at: "metadata.namespace")
-      title: String @field(at: "metadata.title")
-      description: String @field(at: "metadata.description")
-      tags: [String] @field(at: "metadata.tags")
-      links: [EntityLink] @field(at: "metadata.links")
-    }
-
-    type EntityLink {
-      url: String!
-      title: String
-      icon: String
-    }
-
-    extend type Query {
-      entity(kind: String!, name: String!, namespace: String): Entity
-    }
-  `,
+  typeDefs: loadFilesSync(resolvePackagePath('@internal/plugin-graphql', 'typedefs/entity.graphql')),
   resolvers: {
     Query: {
       entity: (
--- plugins/graphql/src/app/modules/user.ts
+++ plugins/graphql/src/app/modules/user.ts
@@ -1,115 +1,13 @@
+import { resolvePackagePath } from '@backstage/backend-common';
 import { Entity, GroupEntity, UserEntity } from '@backstage/catalog-model';
-import { createModule, gql } from 'graphql-modules';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { createModule } from 'graphql-modules';
 import { pascalCase } from 'pascal-case'
 import { resolverProvider } from '../resolver';
 
 export const User = createModule({
   id: `user`,
-  typeDefs: gql`
-    union Owner = User | Team | SubDepartment | Department | Organization
-
-    interface Group {
-      children: [Group]! @relation(type: "parentOf")
-      displayName: String @field(at: "spec.profile.displayName")
-      email: String @field(at: "spec.profile.email")
-      picture: String @field(at: "spec.profile.picture")
-      parent: Group @relation(type: "childOf")
-      members: [User] @relation(type: "hasMember")
-      ownerOf: [Ownable] @relation
-    }
-
-    type Team implements Node & Entity & Group {
-      id: ID!
-
-      name: String!
-      namespace: String
-      title: String
-      description: String
-      tags: [String]
-      links: [EntityLink]
-
-      children: [Group]!
-      displayName: String
-      email: String
-      picture: String
-      parent: SubDepartment
-      members: [User]
-      ownerOf: [Ownable]
-    }
-
-    type SubDepartment implements Node & Entity & Group {
-      id: ID!
-
-      name: String!
-      namespace: String
-      title: String
-      description: String
-      tags: [String]
-      links: [EntityLink]
-
-      children: [Group]!
-      displayName: String
-      email: String
-      picture: String
-      parent: Department
-      members: [User]
-      ownerOf: [Ownable]
-    }
-
-    type Department implements Node & Entity & Group {
-      id: ID!
-
-      name: String!
-      namespace: String
-      title: String
-      description: String
-      tags: [String]
-      links: [EntityLink]
-
-      children: [Group]!
-      displayName: String
-      email: String
-      picture: String
-      parent: Organization
-      members: [User]
-      ownerOf: [Ownable]
-    }
-
-    type Organization implements Node & Entity & Group {
-      id: ID!
-
-      name: String!
-      namespace: String
-      title: String
-      description: String
-      tags: [String]
-      links: [EntityLink]
-
-      children: [Group]!
-      displayName: String
-      email: String
-      picture: String
-      parent: Group
-      members: [User]
-      ownerOf: [Ownable]
-    }
-
-    type User implements Node & Entity {
-      id: ID!
-      name: String!
-      namespace: String
-      title: String
-      description: String
-      tags: [String]
-      links: [EntityLink]
-
-      memberOf: [Group]! @relation
-      displayName: String @field(at: "spec.profile.displayName")
-      email: String @field(at: "spec.profile.email")
-      picture: String @field(at: "spec.profile.picture")
-      ownerOf: [Ownable] @relation
-    }
-  `,
+  typeDefs: loadFilesSync(resolvePackagePath('@internal/plugin-graphql', 'typedefs/user.graphql')),
   providers: [
     resolverProvider({
       accept: (entity: Entity): entity is GroupEntity => entity.kind === 'Group',
