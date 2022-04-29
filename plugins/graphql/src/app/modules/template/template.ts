f15ffadcef4bd64de14124e228e7459e4f9282c5
minkimcello <minkimcello@gmail.com>
2022-04-29 09:32:08 -0400

    Rename graphql plugin name to frontside namespace

--- plugins/graphql/src/app/modules/template/template.ts
+++ plugins/graphql/src/app/modules/template/template.ts
@@ -5,7 +5,7 @@ import GraphQLJSON, { GraphQLJSONObject } from 'graphql-type-json';
 
 export const Template = createModule({
   id: 'Template',
-  typeDefs: loadFilesSync(resolvePackagePath('@internal/plugin-graphql', 'src/app/modules/template/template.graphql')),
+  typeDefs: loadFilesSync(resolvePackagePath('@frontside/backstage-plugin-graphql', 'src/app/modules/template/template.graphql')),
   resolvers: {
     JSON: GraphQLJSON,
     JSONObject: GraphQLJSONObject,
