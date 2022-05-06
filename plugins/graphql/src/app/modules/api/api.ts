cd71bb538937faca59248ed450a1ebac8060b8f5
wKich <w@kich.dev>
2022-05-06 13:35:07 +0500

    Merge graphql modules

--- plugins/graphql/src/app/modules/api/api.ts
+++
@@ -1,8 +0,0 @@
-import { resolvePackagePath } from "@backstage/backend-common";
-import { createModule } from "graphql-modules";
-import { loadFilesSync } from '@graphql-tools/load-files'
-
-export const API = createModule({
-  id: 'API',
-  typeDefs: loadFilesSync(resolvePackagePath('@frontside/backstage-plugin-graphql', 'src/app/modules/api/api.graphql')),
-})
--- plugins/graphql/src/app/modules/user/user.ts
+++
@@ -1,8 +0,0 @@
-import { resolvePackagePath } from '@backstage/backend-common';
-import { loadFilesSync } from '@graphql-tools/load-files';
-import { createModule } from 'graphql-modules';
-
-export const User = createModule({
-  id: `user`,
-  typeDefs: loadFilesSync(resolvePackagePath('@frontside/backstage-plugin-graphql', 'src/app/modules/user/user.graphql')),
-});
