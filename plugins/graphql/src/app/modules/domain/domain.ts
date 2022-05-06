ce73a5b03fbe1b0798aef502439fa0c386acf723
wKich <w@kich.dev>
2022-05-06 13:35:07 +0500

    Merge graphql modules

--- plugins/graphql/src/app/modules/domain/domain.ts
+++
@@ -1,8 +0,0 @@
-import { resolvePackagePath } from '@backstage/backend-common';
-import { loadFilesSync } from '@graphql-tools/load-files';
-import { createModule } from 'graphql-modules';
-
-export const Domain = createModule({
-  id: `domain`,
-  typeDefs: loadFilesSync(resolvePackagePath('@frontside/backstage-plugin-graphql', 'src/app/modules/domain/domain.graphql')),
-});
