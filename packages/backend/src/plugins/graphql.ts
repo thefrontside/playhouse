620704563dfa1cb5345cb5c0bec6014d12b2f571
wKich <w@kich.dev>
2022-05-10 16:21:04 +0500

    Add CI test setup for graphql plugin

--- packages/backend/src/plugins/graphql.ts
+++ packages/backend/src/plugins/graphql.ts
@@ -5,11 +5,6 @@ import { PluginEnvironment } from '../types';
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
-  // Here is where you will add all of the required initialization code that
-  // your backend plugin needs to be able to start!
-
-  // The env contains a lot of goodies, but our router currently only
-  // needs a logger
   return await createRouter({
     logger: env.logger,
     catalog: env.catalog,
