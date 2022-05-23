0fb03690f453a9c7004cbcd06260e6e19284a2aa
minkimcello <minkimcello@gmail.com>
2022-05-23 15:20:39 -0400

    Rename plugin to use frontside namespace

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -54,7 +54,7 @@ import {
 } from '@backstage/plugin-org';
 import { EntityTechdocsContent } from '@backstage/plugin-techdocs';
 import { EmptyState } from '@backstage/core-components';
-import { HumanitecCardComponent } from '@internal/plugin-humanitec';
+import { HumanitecCardComponent } from '@frontside/backstage-plugin-humanitec';
 
 const cicdContent = (
   // This is an example of how you can implement your company's logic in entity page.
