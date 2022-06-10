8e84bb9e1492ab60147e0d634859a984cddbfc71
Jacob Bolda <me@jacobbolda.com>
2022-06-10 10:53:17 -0500

    add graphql backend to graphiql api list

--- packages/app/src/apis.ts
+++ packages/app/src/apis.ts
@@ -9,6 +9,7 @@ import {
   createApiFactory,
   githubAuthApiRef,
   errorApiRef,
+  discoveryApiRef,
 } from '@backstage/core-plugin-api';
 import {
   graphQlBrowseApiRef,
@@ -24,20 +25,38 @@ export const apis: AnyApiFactory[] = [
   ScmAuth.createDefaultApiFactory(),
   createApiFactory({
     api: graphQlBrowseApiRef,
-    deps: { errorApi: errorApiRef, githubAuthApi: githubAuthApiRef },
-    factory: ({ errorApi, githubAuthApi }) =>
+    deps: {
+      errorApi: errorApiRef,
+      githubAuthApi: githubAuthApiRef,
+      discovery: discoveryApiRef,
+    },
+    factory: ({ errorApi, githubAuthApi, discovery }) =>
       GraphQLEndpoints.from([
-        // example, remove in the future?
-        GraphQLEndpoints.create({
-          id: 'swapi',
-          title: 'Star Wars API',
-          url: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
-        }),
+        {
+          id: 'backstage-backend',
+          title: 'Backstage Backend',
+          // we use the lower level object with a fetcher function
+          // as we need to `await` the backend url for the graphql plugin
+          fetcher: async (params: any) => {
+            const graphqlURL = await discovery.getBaseUrl('graphql');
+            return fetch(graphqlURL, {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(params),
+            }).then(res => res.json());
+          },
+        },
         GraphQLEndpoints.github({
           id: 'github',
           title: 'GitHub',
           errorApi,
           githubAuthApi,
+        }),
+        // example, remove in the future?
+        GraphQLEndpoints.create({
+          id: 'swapi',
+          title: 'Star Wars API',
+          url: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
         }),
       ]),
   }),
