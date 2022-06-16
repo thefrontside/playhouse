863703cac3c08e162ebe04695648914d56a629ce
Charles Lowell <cowboyd@frontside.com>
2022-06-16 14:38:39 +0300

    extract types to avoid circular dependencies

--- plugins/graphql/src/app/resolver.ts
+++ plugins/graphql/src/app/resolver.ts
@@ -1,10 +1,4 @@
-import { CatalogApi } from "@backstage/catalog-client";
-import { Loader } from "./loaders";
-
-export interface ResolverContext {
-  loader: Loader
-  catalog: CatalogApi
-}
+import type { ResolverContext } from "./types";
 
 export async function resolveType({ id }: { id: string }, { loader }: ResolverContext): Promise<string | null> {
   const entity = await loader.load(id);
