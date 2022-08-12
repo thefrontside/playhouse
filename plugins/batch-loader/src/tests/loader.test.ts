b93f619f9405a1d0d06a4fa1aa670993e6a66ed7
Dmitriy Lazarev <w@kich.dev>
2022-08-12 13:47:30 +0200

    Use silent logger for batch-loader tests

--- plugins/batch-loader/src/tests/loader.test.ts
+++ plugins/batch-loader/src/tests/loader.test.ts
@@ -1,27 +1,52 @@
 /* eslint-disable func-names */
 /* eslint-disable jest/no-standalone-expect */
 import { describe, beforeAll, it } from '@effection/jest';
-import { DatabaseManager, getRootLogger, ServerTokenManager, SingleHostDiscovery, UrlReaders } from '@backstage/backend-common';
+import { DatabaseManager, ServerTokenManager, SingleHostDiscovery, UrlReaders } from '@backstage/backend-common';
 import { CatalogBuilder, CatalogEnvironment } from '@backstage/plugin-catalog-backend';
 import { ServerPermissionClient } from '@backstage/plugin-permission-node';
 import { ConfigReader } from '@backstage/config';
 import { BatchLoader } from '..'
 import { backstageConfig } from './config';
+import { ensure, once, Operation } from 'effection';
+import { createLogger, Logger, transports } from 'winston';
 
-async function createCatalogPlugin(env: CatalogEnvironment,): Promise<() => Promise<void>> {
-  const builder = await CatalogBuilder.create(env);
+function useCatalogPlugin(env: CatalogEnvironment): Operation<void> {
+  return {
+    name: "CatalogPlugin",
+    *init() {
+       const builder = CatalogBuilder.create(env);
+       const { processingEngine } = yield builder.build();
 
-  const { processingEngine } = await builder.build();
-  await processingEngine.start();
-  return () => processingEngine.stop();
+       yield processingEngine.start();
+       yield ensure(() => processingEngine.stop());
+     }
+  }
+}
+
+function useLogger(): Operation<Logger> {
+  return {
+    name: "Logger",
+    *init() {
+      const transport = new transports.Console();
+      const logger = createLogger({
+        level: 'error',
+        transports: [transport],
+      });
+      yield ensure(function* () {
+        logger.end();
+        logger.on('error', function () { /* noop */ });
+        yield once(logger, 'finish')
+      });
+      return logger
+    }
+  }
 }
 
 describe('loading entities in a batch', () => {
   let loader: BatchLoader
-  let teardown: () => Promise<void>;
 
   beforeAll(function* () {
-    const logger = getRootLogger();
+    const logger = yield useLogger()
     const config = new ConfigReader(backstageConfig);
     const reader = UrlReaders.default({ logger, config });
     const databaseManager = DatabaseManager.fromConfig(config);
@@ -31,7 +56,7 @@ describe('loading entities in a batch', () => {
       discovery,
       tokenManager,
     });
-    teardown = yield createCatalogPlugin({
+    yield useCatalogPlugin({
       logger,
       database: databaseManager.forPlugin('catalog'),
       config,
@@ -41,10 +66,6 @@ describe('loading entities in a batch', () => {
     loader = new BatchLoader({ databaseManager, logger })
     yield loader.init()
   });
-
-  afterAll(function *() {
-    yield teardown()
-  })
 
   it.eventually('can look up a entity by stringified ref', function* () {
     const [teamA] = yield loader.getEntitiesByRefs(['group:default/team-a'])
