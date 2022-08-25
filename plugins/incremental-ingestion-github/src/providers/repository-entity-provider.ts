925a13b9ecfc6fff6a35c9ea5fa6a9df2b41fafb
Taras <tarasm@gmail.com>
2022-08-25 17:44:16 -0400

    Use class based APIs for the repository entity provider

--- plugins/incremental-ingestion-github/src/providers/repository-entity-provider.ts
+++ plugins/incremental-ingestion-github/src/providers/repository-entity-provider.ts
@@ -1,37 +1,11 @@
 import { ANNOTATION_LOCATION, ANNOTATION_ORIGIN_LOCATION, DEFAULT_NAMESPACE, stringifyEntityRef } from "@backstage/catalog-model";
-import { GithubCredentialsProvider, GitHubIntegration } from '@backstage/integration';
-import { IncrementalEntityProvider } from "@frontside/backstage-plugin-incremental-ingestion-backend";
+import { Config } from "@backstage/config";
+import { DefaultGithubCredentialsProvider, GitHubIntegration, ScmIntegrations } from '@backstage/integration';
+import type { EntityIteratorResult, IncrementalEntityProvider } from "@frontside/backstage-plugin-incremental-ingestion-backend";
 import { graphql } from '@octokit/graphql';
+import assert from 'assert-ts';
 import slugify from 'slugify';
-import { Logger } from "winston";
 import type { RepositorySearchQuery } from "./repository-entity-provider.__generated__";
-
-interface Context {
-  client: typeof graphql;
-  url: string;
-}
-
-interface Options {
-  id: string;
-  /**
-   * Advanced search query that can be generated at
-   * https://github.com/search/advanced
-   */
-  searchQuery?: string;
-  /**
-   * Used to retrieve token that will be used by the client
-   */
-  credentialsProvider: GithubCredentialsProvider;
-  /**
-   * Provides base URL for GitHub GraphQL API
-   */
-  integration: GitHubIntegration;
-  logger: Logger
-}
-
-interface Cursor {
-  cursor: string | null;
-}
 
 const REPOSITORY_SEARCH_QUERY = /* GraphQL */`
   query RepositorySearch($searchQuery: String!, $cursor: String) {
@@ -89,78 +63,117 @@ const REPOSITORY_SEARCH_QUERY = /* GraphQL */`
   }
 `;
 
-export function createGithubRepositoryEntityProvider({
-  id,
-  credentialsProvider,
-  integration,
-  searchQuery = "created:>1970-01-01",
-}: Options): IncrementalEntityProvider<Cursor, Context> {
+interface GithubRepositoryEntityProviderOptions {
+  host: string;
+  config: Config;
+  searchQuery: string;
+}
 
-  return {
-    getProviderName() {
-      return `GithubRepository:${id}`
-    },
-    async around(burst) {
-      const url = `https://${integration.config.host}`;
+interface Context {
+  client: typeof graphql;
+  url: string;
+}
 
-      const { headers } = await credentialsProvider.getCredentials({
-        url,
-      });
+interface Cursor {
+  cursor: string | null;
+}
 
-      const client = graphql.defaults({
-        baseUrl: integration.config.apiBaseUrl,
-        headers,
-      });
+interface GithubRepositoryEntityProviderConstructorOptions { 
+  credentialsProvider: DefaultGithubCredentialsProvider; 
+  host: string; 
+  integration: GitHubIntegration; 
+  searchQuery: string; 
+}
 
-      await burst({ client, url })
-    },
-    async next({ client, url }, { cursor } = { cursor: null }) {
+export class GithubRepositoryEntityProvider implements IncrementalEntityProvider<Cursor, Context> {
+  private host: string;
+  private credentialsProvider: DefaultGithubCredentialsProvider;
+  private integration: GitHubIntegration;
+  private searchQuery: string;
 
-      const data = await client<RepositorySearchQuery>(REPOSITORY_SEARCH_QUERY,
-        {
-          cursor,
-          searchQuery,
-        }
-      );
+  static create({ host, config, searchQuery = "created:>1970-01-01" }: GithubRepositoryEntityProviderOptions) {
+    const integrations = ScmIntegrations.fromConfig(config);
+    const credentialsProvider = DefaultGithubCredentialsProvider.fromIntegrations(integrations);
+    const integration = integrations.github.byHost(host);
 
-      const location = `url:${url}`;
+    assert(integration !== undefined, `Missing Github integration for ${host}`);
 
-      const entities = data.search.nodes?.flatMap(node => node?.__typename === 'Repository' ? [node] : [])
-        .map(node => ({
-          entity: {
-            apiVersion: 'backstage.io/v1beta1',
-            kind: 'GithubRepository',
-            metadata: {
-              namespace: DEFAULT_NAMESPACE,
-              name: normalizeEntityName(node.nameWithOwner),
-              description: node.description ?? '',
-              annotations: {
-                [ANNOTATION_LOCATION]: location,
-                [ANNOTATION_ORIGIN_LOCATION]: location,
-              },
-            },
-            spec: {
-              url: node.url,
-              owner: stringifyEntityRef({
-                kind: `Github${node.owner.__typename}`,
-                namespace: DEFAULT_NAMESPACE,
-                name: node.owner.login
-              }),
-              nameWithOwner: node.nameWithOwner,
-              languages: node.languages?.nodes?.flatMap(_node => _node?.__typename === 'Language' ? [_node] : []).map(_node => _node.name) ?? [],
-              topics: node.repositoryTopics?.nodes?.flatMap(_node => _node?.__typename === 'RepositoryTopic' ? [_node] : []).map(_node => _node.topic.name) ?? [],
-              visibility: node.visibility,
+    return new GithubRepositoryEntityProvider({ credentialsProvider, host, integration, searchQuery })
+  }
+
+  private constructor(options: GithubRepositoryEntityProviderConstructorOptions) {
+    this.credentialsProvider = options.credentialsProvider;
+    this.host = options.host;
+    this.integration = options.integration;
+    this.searchQuery = options.searchQuery;
+  }
+
+  getProviderName() {
+    return `GithubRepository:${this.host}`;
+  }
+
+  async around(burst: (context: Context) => Promise<void>) {
+
+    const url = `https://${this.host}`;
+
+    const { headers } = await this.credentialsProvider.getCredentials({
+      url,
+    });
+
+    const client = graphql.defaults({
+      baseUrl: this.integration.config.apiBaseUrl,
+      headers,
+    });
+
+    await burst({ client, url })
+  }
+
+  async next({ client, url }: Context, { cursor }: Cursor = { cursor: null }): Promise<EntityIteratorResult<Cursor>> {
+
+    const data = await client<RepositorySearchQuery>(REPOSITORY_SEARCH_QUERY,
+      {
+        cursor,
+        searchQuery: this.searchQuery,
+      }
+    );
+
+    const location = `url:${url}`;
+
+    const entities = data.search.nodes?.flatMap(node => node?.__typename === 'Repository' ? [node] : [])
+      .map(node => ({
+        entity: {
+          apiVersion: 'backstage.io/v1beta1',
+          kind: 'GithubRepository',
+          metadata: {
+            namespace: DEFAULT_NAMESPACE,
+            name: normalizeEntityName(node.nameWithOwner),
+            description: node.description ?? '',
+            annotations: {
+              [ANNOTATION_LOCATION]: location,
+              [ANNOTATION_ORIGIN_LOCATION]: location,
             },
           },
-          locationKey: this.getProviderName(),
-        }));
+          spec: {
+            url: node.url,
+            owner: stringifyEntityRef({
+              kind: `Github${node.owner.__typename}`,
+              namespace: DEFAULT_NAMESPACE,
+              name: node.owner.login
+            }),
+            nameWithOwner: node.nameWithOwner,
+            languages: node.languages?.nodes?.flatMap(_node => _node?.__typename === 'Language' ? [_node] : []).map(_node => _node.name) ?? [],
+            topics: node.repositoryTopics?.nodes?.flatMap(_node => _node?.__typename === 'RepositoryTopic' ? [_node] : []).map(_node => _node.topic.name) ?? [],
+            visibility: node.visibility,
+          },
+        },
+        locationKey: this.getProviderName(),
+      }));
 
-      return {
-        done: !data.search.pageInfo.hasNextPage,
-        cursor: { cursor: data.search.pageInfo.endCursor ?? null },
-        entities: entities ?? []
-      };
-    }
+    return {
+      done: !data.search.pageInfo.hasNextPage,
+      cursor: { cursor: data.search.pageInfo.endCursor ?? null },
+      entities: entities ?? []
+    };
   }
 }
 
