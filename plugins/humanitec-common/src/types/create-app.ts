509d9dbfd54cf10a2cd99f7fd856a7ed36574519
minkimcello <minkimcello@gmail.com>
2022-09-12 16:26:18 -0400

    Remove unnecessary object wrapper for variables

--- plugins/humanitec-common/src/types/create-app.ts
+++ plugins/humanitec-common/src/types/create-app.ts
@@ -25,9 +25,7 @@ export const Module = object({
         limits: Resource.optional(),
         requests: Resource.optional(),
       }).optional(),
-      variables: object({
-        Variables
-      }),
+      variables: Variables,
       volume_mounts: object({}),
       files: object({})
     })).optional(),
