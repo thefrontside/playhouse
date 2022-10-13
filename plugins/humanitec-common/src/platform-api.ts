5af97a30b094b3c86f4a4b983e712e983e68b141
Charles Lowell <cowboyd@frontside.com>
2022-10-13 17:05:35 -0500

    Add environments call

---
+++ plugins/humanitec-common/src/platform-api.ts
@@ -0,0 +1,45 @@
+import type { PlatformApi } from '@frontside/backstage-plugin-platform-backend';
+import type { Entity } from '@backstage/catalog-model';
+
+import { HUMANITEC_APP_ID_ANNOTATION, HUMANITEC_ORG_ID_ANNOTATION } from './constants';
+import { createHumanitecClient } from './clients/humanitec';
+
+export function createHumanitecPlatformApi({ token }: { token: string }): PlatformApi {
+
+  return {
+    async getEnvironments(ref) {
+      let entity = await ref.load();
+      let { appId, orgId } = getHumanitecMetadata(entity);
+      let client = createHumanitecClient({ token, orgId });
+      let environments = await client.getEnvironments(appId);
+      return {
+        hasNextPage: false,
+        hasPreviousPage: false,
+        beginCursor: '',
+        endCursor: '',
+        items: environments.map(env => ({
+          cursor: '',
+          value: {
+            id: env.id,
+            name: env.name,
+          }
+        })),
+      };
+    }
+  }
+}
+
+function getHumanitecMetadata(entity: Entity) {
+  let orgId = entity.metadata.annotations[HUMANITEC_ORG_ID_ANNOTATION];
+  let appId = entity.metadata.annotations[HUMANITEC_APP_ID_ANNOTATION];
+
+  if (!orgId) {
+    throw new Error(`${entity.kind}:${entity.metadata.name} is not a humanitec entity`);
+
+  }
+  if (!appId) {
+    throw new Error(`${entity.kind}:${entity.metadata.name} is not a humanitec entity`);
+
+  }
+  return { orgId, appId };
+}
--- plugins/platform-backend/cli/cli.ts
+++ plugins/platform-backend/cli/cli.ts
@@ -1,4 +1,4 @@
-import { path, yaml, Entity, Command } from "./deps.ts";
+import { Command, Entity, path, yaml } from "./deps.ts";
 
 export interface CLIOptions {
   name: string;
@@ -9,7 +9,7 @@ export interface CLIOptions {
 }
 
 class MainError extends Error {
-  name = 'Mainerror';
+  name = "Mainerror";
 }
 
 export async function cli(options: CLIOptions) {
@@ -20,8 +20,11 @@ export async function cli(options: CLIOptions) {
     .name(name)
     .version(() => `\narchitecture: ${target}\nbackstage: ${apiURL}`)
     .description(description)
-    .command('info', 'display info about a backstage component entity.')
-    .option('-c --component <component:string>', 'The backstage component entity')
+    .command("info", "display info about a backstage component entity.")
+    .option(
+      "-c --component <component:string>",
+      "The backstage component entity",
+    )
     .action(async ({ component }) => {
       let ref = await findEntityContext(component);
       let response: Response;
@@ -33,55 +36,95 @@ export async function cli(options: CLIOptions) {
       if (!response) {
         throw new MainError(`no response from server`);
       } else if (response.ok) {
-        await Deno.stdout.write(new TextEncoder().encode(await response.text()));
+        await Deno.stdout.write(
+          new TextEncoder().encode(await response.text()),
+        );
       } else {
         if (response.status === 404) {
           throw new MainError(`unknown component '${ref}'`);
         } else {
-          throw new MainError(`communication error with backstage server: ${response.status} ${response.statusText}`);
+          throw new MainError(
+            `communication error with backstage server: ${response.status} ${response.statusText}`,
+          );
         }
       }
     })
-
-    try {
-      await cmd.parse(args);
-    } catch (error) {
-      if (error instanceof MainError) {
-        console.log(error.message);
-      } else {
-        throw error;
+    .command(
+      "environments",
+      "list enviroments in which a component is deployed",
+    )
+    .option("-c --component <component:string>", "the component to query")
+    .action(async ({ component }) => {
+      let ref = await findEntityContext(component);
+      let response: Response;
+      try {
+        response = await get(`components/${ref}/environments`);
+      } catch (error) {
+        throw new MainError(error.message);
       }
+      if (!response) {
+        throw new MainError(`no response from server`);
+      } else if (response.ok) {
+        await Deno.stdout.write(
+          new TextEncoder().encode(await response.text()),
+        );
+      } else {
+        if (response.status === 404) {
+          throw new MainError(`unknown component '${ref}'`);
+        } else {
+          throw new MainError(
+            `communication error with backstage server: ${response.status} ${response.statusText}`,
+          );
+        }
+      }
+    });
+
+  try {
+    await cmd.parse(args);
+  } catch (error) {
+    if (error instanceof MainError) {
+      console.log(error.message);
+    } else {
+      throw error;
     }
+  }
 }
 
 async function findEntityContext(component?: string): Promise<string> {
   if (component) {
     return component;
   }
-  let catalogInfoYaml = await findAndRead("catalog-info.yml", "catalog-info.yaml");
+  let catalogInfoYaml = await findAndRead(
+    "catalog-info.yml",
+    "catalog-info.yaml",
+  );
   if (catalogInfoYaml.found) {
     let [info] = yaml.parseAll(catalogInfoYaml.content) as Iterable<Entity>;
     if (info && info.metadata?.name) {
       return info.metadata.name;
     }
   } else {
-    throw new MainError('unable to determine the component. You can set it explicitly by passing the `--component` flag');
+    throw new MainError(
+      "unable to determine the component. You can set it explicitly by passing the `--component` flag",
+    );
   }
 
-  return '';
+  return "";
 }
 
 type Find = {
-  found: false,
+  found: false;
 } | {
-  found: true,
+  found: true;
   content: string;
-}
+};
 async function findAndRead(...paths: string[]): Promise<Find> {
-  for (let cwd = Deno.cwd(); cwd !== '/'; cwd = path.join(cwd, '..')) {
+  for (let cwd = Deno.cwd(); cwd !== "/"; cwd = path.join(cwd, "..")) {
     for (let path of paths) {
       try {
-        let content = new TextDecoder().decode(await Deno.readFile(`${cwd}/${path}`));
+        let content = new TextDecoder().decode(
+          await Deno.readFile(`${cwd}/${path}`),
+        );
         return { found: true, content };
       } catch (error) {
         if (!(error instanceof Deno.errors.NotFound)) {
