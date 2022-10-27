9ccaba9ea1efa2ea652153fc74ad6a8a05fd53a1
Jacob Bolda <me@jacobbolda.com>
2022-10-27 14:37:06 -0500

    skip looking up user in the catalog

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -1,3 +1,7 @@
+import {
+  DEFAULT_NAMESPACE,
+  stringifyEntityRef,
+} from '@backstage/catalog-model';
 import { createRouter, providers } from '@backstage/plugin-auth-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
@@ -18,19 +22,45 @@ export default async function createPlugin({
     providerFactories: {
       auth0: providers.auth0.create({
         signIn: {
-          resolver: async (info, ctx) => {
-            const {
-              profile: { email },
-            } = info;
-
-            if (!email) {
+          resolver: async ({ profile }, ctx) => {
+            if (!profile?.email) {
               throw new Error('User profile contained no email');
             }
 
-            const [name] = email.split('@');
+            const [name, domain] = profile?.email?.split('@');
+            // when we want to find the user in the catalog,
+            //  use this function
+            // return ctx.signInWithCatalogUser({
+            //   entityRef: { name },
+            // });
 
-            return ctx.signInWithCatalogUser({
-              entityRef: { name },
+            // otherwise we can sign in without having the
+            //  user in the catalog
+            //  https://backstage.io/docs/auth/identity-resolver#sign-in-without-users-in-the-catalog
+            // Split the email into the local part and the domain.
+
+            // Next we verify the email domain. It is recommended to include this
+            // kind of check if you don't look up the user in an external service.
+            if (
+              domain !== 'frontside.com' &&
+              config.getString('auth.environment') === 'production'
+            ) {
+              throw new Error(
+                `Login failed, this email ${profile.email} does not belong to the expected domain`,
+              );
+            }
+
+            // By using `stringifyEntityRef` we ensure that the reference is formatted correctly
+            const userEntity = stringifyEntityRef({
+              kind: 'User',
+              name,
+              namespace: DEFAULT_NAMESPACE,
+            });
+            return ctx.issueToken({
+              claims: {
+                sub: userEntity,
+                ent: [userEntity],
+              },
             });
           },
         },
