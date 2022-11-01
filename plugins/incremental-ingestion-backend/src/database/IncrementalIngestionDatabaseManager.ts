8b7d0559e88fc5a975c0a7603bfe29ff93529d08
Paul Cowan <paul.cowan@cutting.scot>
2022-11-01 15:30:49 +0000

    remove backstage comment headers

--- plugins/incremental-ingestion-backend/src/database/IncrementalIngestionDatabaseManager.ts
+++ plugins/incremental-ingestion-backend/src/database/IncrementalIngestionDatabaseManager.ts
@@ -1,18 +1,4 @@
-/*
- * Copyright 2022 The Backstage Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 import { Knex } from 'knex';
 import type { DeferredEntity } from '@backstage/plugin-catalog-backend';
 import { stringifyEntityRef } from '@backstage/catalog-model';
