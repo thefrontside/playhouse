2140b71a4938cb05afc43beaf5272d188cc5d3f9
Paul Cowan <paul.cowan@cutting.scot>
2022-11-21 17:02:35 +0000

    add Button props to yaml

--- packages/app/src/components/yaml-editor/YamlEditor.tsx
+++ packages/app/src/components/yaml-editor/YamlEditor.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import Editor, { OnChange } from "@monaco-editor/react";
-import type { EditorProps, OnMount, BeforeMount } from "@monaco-editor/react";
+import Editor from "@monaco-editor/react";
+import type { EditorProps, OnMount, BeforeMount, OnChange } from "@monaco-editor/react";
 
-export type YAMLEditorProps = Pick<EditorProps, 'onMount' | 'onChange' | 'beforeMount' | 'defaultValue'>;
+export type YAMLEditorProps = Pick<EditorProps, 'onMount' | 'onChange' | 'beforeMount' | 'defaultValue' | 'value'>;
 export type MonacoEditor = Parameters<OnMount>[0];
 export type OnChangeArgs = Parameters<OnChange>;
 export type Monaco = Parameters<BeforeMount>;
 
-export function YAMLEditor({ onMount, onChange, beforeMount, defaultValue }: YAMLEditorProps): JSX.Element {
+export function YAMLEditor({ onMount, onChange, beforeMount, defaultValue, value }: YAMLEditorProps): JSX.Element {
   return (
-      <Editor
+    <Editor
       height="50vh"
       defaultLanguage="yml"
       defaultValue={defaultValue}
       onMount={onMount}
       onChange={onChange}
       beforeMount={beforeMount}
+      value={value}
     />
   );
 }
