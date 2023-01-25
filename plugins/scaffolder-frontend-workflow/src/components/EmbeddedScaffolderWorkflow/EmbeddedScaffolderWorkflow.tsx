4994ef940655e7b54b2c78904d5b992cf9d87a17
Paul Cowan <paul.cowan@cutting.scot>
2023-01-25 16:12:39 +0000

    remove comments

--- plugins/scaffolder-frontend-workflow/src/components/EmbeddedScaffolderWorkflow/EmbeddedScaffolderWorkflow.tsx
+++ plugins/scaffolder-frontend-workflow/src/components/EmbeddedScaffolderWorkflow/EmbeddedScaffolderWorkflow.tsx
@@ -1,27 +1,8 @@
-/*
- * Copyright 2022 The Backstage Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React, { useState, useCallback } from 'react';
 import type { ReactNode } from 'react';
 import { EmbeddableWorkflow, type WorkflowProps } from '@backstage/plugin-scaffolder-react/alpha';
 import { Box, Button } from '@material-ui/core';
 
-/**
- * @alpha
- */
 export type EmbeddedScaffolderWorkflowProps = WorkflowProps & {
   onError(error: Error | undefined): JSX.Element | null;
   frontPage: ReactNode;
@@ -36,7 +17,6 @@ type OnCompleteArgs = Parameters<WorkflowProps['onCreate']>[0];
 
 /**
  * Allows the EmbeddableWorkflow to be called from outside of a normal scaffolder workflow
- * @alpha
  */
 export function EmbeddedScaffolderWorkflow({
   frontPage,
