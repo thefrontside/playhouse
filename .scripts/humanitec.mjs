03cca54032b6e7fce3e096562e4a2be33ffe5332
Jacob Bolda <me@jacobbolda.com>
2023-02-02 13:26:15 -0600

    add cron to spin down envs

--- .scripts/humanitec.mjs
+++ .scripts/humanitec.mjs
@@ -1,20 +1,32 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { main, createTask, fetch } from 'effection';
 import crypto from 'node:crypto';
+import { main, createTask, fetch } from 'effection';
+import { Octokit } from 'octokit';
 
 main(function* () {
+  if (!process.env.HUMANITEC_ORG_ID)
+    throw new Error('HUMANITEC_ORG_ID env need to be set');
+
   let task = createTask(function* (scope) {
     const program = new Command();
     program
       .command('get-environments')
+      .alias('get-envs')
       .action(async () => await scope.run(getEnvironments()));
     program
       .command('clone-environment')
+      .alias('clone-env')
       .argument('[newEnvName]', 'name of new environment')
       .action(
         async newEnvName => await scope.run(cloneEnvironment({ newEnvName })),
       );
+    program
+      .command('tidy-previews')
+      .action(async () => await scope.run(tidyPreviews()));
+    program
+      .command('replicas')
+      .action(async () => await scope.run(getReplicas()));
 
     yield program.parseAsync();
   });
@@ -24,10 +36,8 @@ main(function* () {
   });
 });
 
-function* getEnvironments() {
+function* getEnvironments({ silent = false }) {
   const { HUMANITEC_ORG_ID } = process.env;
-  if (!process.env.HUMANITEC_ORG_ID)
-    throw new Error('HUMANITEC_ORG_ID env need to be set');
 
   let url = `https://api.humanitec.io/orgs/${HUMANITEC_ORG_ID}/apps/backstage/envs`;
   let result = yield fetch(url, {
@@ -35,7 +45,7 @@ function* getEnvironments() {
     ...createHeaders(),
   });
 
-  yield processResponse({ result });
+  return yield processResponse({ result, silent });
 }
 
 function* cloneEnvironment({ newEnvName }) {
@@ -95,6 +105,112 @@ function* cloneEnvironment({ newEnvName }) {
   });
 }
 
+function* tidyPreviews() {
+  let envs = yield getEnvironments({ silent: true });
+  let previews = envs.filter(env => env.type === 'preview');
+
+  let deletedEnvs = yield deleteUnusedEnvironments({ previews });
+  let deletedEnvsID = deletedEnvs.map(env => env.id);
+  let remainingEnvs = previews.filter(env => !deletedEnvsID.includes(env.id));
+  let spunDownEnvs = yield spinDownEnvironment({ previews: remainingEnvs });
+}
+
+function* deleteUnusedEnvironments({ previews }) {
+  let listOfPRs = yield queryRepoPRs();
+  let listOfPRNumbers = listOfPRs.map(pull => `pr${pull.number}`);
+  let previewsToDelete = previews.filter(
+    preview => !listOfPRNumbers.includes(preview.id),
+  );
+
+  const { HUMANITEC_ORG_ID } = process.env;
+  for (let preview of previewsToDelete) {
+    let url = `https://api.humanitec.io/orgs/${HUMANITEC_ORG_ID}/apps/backstage/envs/${preview.id}`;
+    let result = yield fetch(url, {
+      method: 'DELETE',
+      ...createHeaders(),
+    });
+
+    if (result.status === 204) {
+      console.log(`deleting ${preview.id}`);
+    } else {
+      console.log(
+        `attempted to delete ${preview.id}, but an issue arose, skipping`,
+      );
+      console.error(result);
+    }
+  }
+
+  return previewsToDelete;
+}
+
+function* queryRepoPRs() {
+  if (!process.env.GITHUB_TOKEN)
+    throw new Error('GITHUB_TOKEN is required to query for PRs');
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  const { data } = yield octokit.rest.pulls.list({
+    owner: 'thefrontside',
+    repo: 'playhouse',
+    state: 'open',
+  });
+
+  return data;
+}
+
+function* spinDownEnvironment({ previews }) {
+  const { HUMANITEC_ORG_ID } = process.env;
+  let envsToSpinDown = [];
+
+  for (let preview of previews) {
+    if (preview.last_deploy) {
+      let lastDeployDT = new Date(preview.last_deploy.created_at);
+
+      let minutesSinceLastDeploy = (new Date() - lastDeployDT) / 1000 / 60;
+      let minutesInDay = 24 * 60 - 9999;
+      if (minutesSinceLastDeploy > minutesInDay) envsToSpinDown.push(preview);
+    }
+  }
+
+  for (let preview of envsToSpinDown) {
+    let url = `https://api.humanitec.io/orgs/${HUMANITEC_ORG_ID}/apps/backstage/envs/${preview.id}/runtime/replicas`;
+    // let url = `https://api.humanitec.io/orgs/${HUMANITEC_ORG_ID}/apps/backstage/envs/${preview.id}/runtime`;
+    // method: 'GET',
+    let result = yield fetch(url, {
+      method: 'PATCH',
+      ...createHeaders(),
+      body: JSON.stringify({
+        backstage: 0,
+      }),
+    });
+
+    if (result.status === 204) {
+      console.log(`spinning down ${preview.id}`);
+    } else {
+      console.log(
+        `attempted to spin down ${preview.id}, but an issue arose, skipping`,
+      );
+      console.error(result);
+    }
+  }
+
+  return envsToSpinDown;
+}
+
+function* getReplicas() {
+  let envs = yield getEnvironments({ silent: true });
+  const { HUMANITEC_ORG_ID } = process.env;
+
+  for (let env of envs) {
+    let url = `https://api.humanitec.io/orgs/${HUMANITEC_ORG_ID}/apps/backstage/envs/${env.id}/runtime`;
+    let result = yield fetch(url, {
+      method: 'GET',
+      ...createHeaders(),
+    });
+
+    yield processResponse({ result });
+  }
+}
+
 function createHeaders({ extraHeaders = {} } = { extraHeaders: {} }) {
   if (!process.env.HUMANITEC_TOKEN)
     throw new Error('HUMANITEC_TOKEN env need to be set');
@@ -108,15 +224,14 @@ function createHeaders({ extraHeaders = {} } = { extraHeaders: {} }) {
   };
 }
 
-function* processResponse({ result, label }) {
+function* processResponse({ result, label, silent, throwOnError = true }) {
   let response = yield result.json();
   if (result.status < 400) {
-    if (label) console.log(label);
-    console.dir(response);
+    if (label && !silent) console.log(label);
+    if (!silent) console.dir(response, { depth: 6 });
     return response;
   } else {
-    console.error(result);
     console.error(response);
-    throw new Error(response.message);
+    if (throwOnError) throw new Error(response.message);
   }
 }
