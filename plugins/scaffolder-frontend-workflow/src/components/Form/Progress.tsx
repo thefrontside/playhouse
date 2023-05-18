68b5c2379d9b64a28ab30301d5bdcc164cabbc4b
Paul Cowan <paul.cowan@cutting.scot>
2023-05-18 20:33:02 +0100

    add EventStream

--- plugins/scaffolder-frontend-workflow/src/components/Form/Progress.tsx
+++ plugins/scaffolder-frontend-workflow/src/components/Form/Progress.tsx
@@ -1,6 +1,6 @@
 import { useTaskEventStream } from '@backstage/plugin-scaffolder-react';
 import { Progress } from '@backstage/core-components';
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { WorkflowProps } from '@backstage/plugin-scaffolder-react/dist/alpha';
 
 type Props = {
@@ -10,6 +10,27 @@ type Props = {
 
 export function TaskProgress({ taskId, onComplete, onError }: Props) {
   const taskStream = useTaskEventStream(taskId);
+
+  const steps = useMemo(
+    () =>
+      taskStream.task?.spec.steps.map(step => ({
+        ...step,
+        ...taskStream?.steps?.[step.id],
+      })) ?? [],
+    [taskStream],
+  );
+
+  const activeStep = useMemo(() => {
+    for (let i = steps.length - 1; i >= 0; i--) {
+      if (steps[i].status !== 'open') {
+        return i;
+      }
+    }
+
+    return 0;
+  }, [steps]);
+
+  console.log(activeStep, steps)
 
   useEffect(() => {
     if (taskStream.error) {
